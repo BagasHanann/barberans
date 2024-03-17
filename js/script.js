4cf13d8485aea19c223bd980fdc650b693357832
Bagas Hananto <bagashp01@gmail.com>
2024-03-18 02:59:39 +0700

    Add product page

--- js/script.js
+++ js/script.js
@@ -5,8 +5,22 @@ document.querySelector('#hamburger').onclick = () => {
 };
 
 const hamburger = document.querySelector('#hamburger');
-document.addEventListener('click', function () {
-	if (!hamburger.contains(e.target) && navMenu.contains('active')) {
+const searchButton = document.querySelector('#search-button');
+
+document.addEventListener('click', function (e) {
+	if (!hamburger.contains(e.target) && !navMenu.contains(e.target)) {
 		navMenu.classList.remove('active');
 	}
+	if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+		searchForm.classList.remove('active');
+	}
 });
+
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+	e.preventDefault();
+	searchForm.classList.toggle('active');
+	searchBox.focus();
+};
